a12e6446547c5dc474c358c39237acc2c8dbf4af
Aditya Ch <ch.aditya1996@gmail.com>
2020-11-07 18:37:17 -0800

    Details component implemented

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,6 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { DetailsModel } from 'src/app/models/DetailsModel';
+import { NewsModel } from 'src/app/models/NewsModel';
+import { OutlookModel } from 'src/app/models/OutlookModel';
+import { SummaryModel } from 'src/app/models/SummaryModel';
 import { DetailsService } from 'src/app/services/details.service';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { Article } from 'src/app/models/Article';
+import * as $ from "jquery";
+import { WatchlistService } from 'src/app/services/watchlist.service';
+import { FormControl } from '@angular/forms';
+import { PortfolioService } from 'src/app/services/portfolio.service';
+import { Utility } from 'src/app/common/Utility';
+
+
+import * as Highcharts from 'highcharts/highstock';
+
+import more from 'highcharts/highcharts-more';
+import IndicatorsCore from 'highcharts/indicators/indicators';
+import HC_stock from 'highcharts/modules/stock';
+import vbp from 'highcharts/indicators/volume-by-price';
+import { StockInfoModel } from 'src/app/models/StockInfoModel';
+import { StockInfo } from 'src/app/models/StockInfo';
+
+more(Highcharts);
+IndicatorsCore(Highcharts);
+vbp(Highcharts);
+HC_stock(Highcharts);
+
 
 @Component({
     selector: 'app-details',
@@ -9,19 +36,321 @@ import { DetailsService } from 'src/app/services/details.service';
 })
 export class DetailsComponent implements OnInit {
 
+    details: DetailsModel;
     ticker: string;
     showSpinner: boolean;
+    isFavorite: boolean;
+
+    change: number;
+    changeStr: string;
+    changePer: number;
+    changePerStr: string;
+    current_date: Date;
+    noChange = false;
+    greenColorVar = true;
+    marketOpen = true;
+    marketClosedOn: Date;
+
+    closeResult = '';
+
+    clickedArticle: Article;
+    twitterContent: string;
+    fbContent: string;
+
+    costprice: number;
+    costpriceStr: string;
+
+    stockbuyable: boolean;
+
+    myControl = new FormControl();
+
+    Highcharts: typeof Highcharts = Highcharts;
+    chartOptions: Highcharts.Options;
+    updateFlag: boolean = false;
 
     constructor(private route: ActivatedRoute,
-        private detailsService: DetailsService) { }
+        private detailsService: DetailsService,
+        private modalService: NgbModal,
+        private watchlistService: WatchlistService,
+        private portfolioService: PortfolioService) { }
+
+    setFavorite() {
+        if (this.isFavorite) {
+            this.isFavorite = false;
+
+            // take care not to show both alerts at the same time!
+            $('.watchlist-add-alert').hide()
+
+            $('.watchlist-remove-alert').show();
+            setTimeout(function () {
+                if ($('.watchlist-remove-alert').length > 0) {
+                    $('.watchlist-remove-alert').hide();
+                }
+            }, 2000);
+            this.watchlistService.removeTickerFromWatchlist(this.ticker);
+        }
+        else {
+            this.isFavorite = true;
+
+            // take care not to show both alerts at the same time!
+            $('.watchlist-remove-alert').hide()
+
+            $('.watchlist-add-alert').show();
+            setTimeout(function () {
+                if ($('.watchlist-add-alert').length > 0) {
+                    $('.watchlist-add-alert').hide();
+                }
+            }, 2000);
+            this.watchlistService.addTickerToWatchlist(this.ticker);
+        }
+    }
 
     ngOnInit(): void {
+        this.details = new DetailsModel();
+        this.details.outlookModel = new OutlookModel();
+        this.details.summaryModel = new SummaryModel();
+        this.details.newsModel = new NewsModel();
+
+        this.current_date = new Date();
         this.showSpinner = true;
+
+        this.costprice = 0;
+        this.costpriceStr = "0.00";
+        this.stockbuyable = false;
+
         this.ticker = this.route.snapshot.paramMap.get('ticker');
+
+        this.isFavorite = this.watchlistService.contains(this.ticker);
+
         this.detailsService.getDetails(this.ticker).subscribe(result => {
-            console.log(result);
+            this.details = result;
+            this.change = this.details.summaryModel.lastPrice - this.details.summaryModel.previousClosingPrice;
+            this.changeStr = Utility.beautify(this.change);
+            if (this.change > 0) {
+                this.greenColorVar = true;
+            }
+            else if (this.change < 0) {
+                this.greenColorVar = false;
+            }
+            else {
+                this.noChange = true;
+            }
+            this.changePer = (this.change * 100) / this.details.summaryModel.previousClosingPrice;
+            this.changePerStr = Utility.beautify(this.changePer);
+
+            if ((this.current_date.getTime() - (new Date(this.details.summaryModel.timestamp).getTime())) > 60000) {
+                this.marketOpen = false;
+                this.marketClosedOn = new Date(this.details.summaryModel.timestamp);
+            }
+            else {
+                this.marketOpen = true;
+            }
+
+            this.updateFlag = true;
+
+            this.plotHighChart(this.details.historicalStockInfo);
+
             this.showSpinner = false;
+        });
+
+
+    }
+
+    open(content) {
+        this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
+            this.closeResult = `Closed with: ${result}`;
+        }, (reason) => {
+            this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
         });
     }
 
+    private getDismissReason(reason: any): string {
+        if (reason === ModalDismissReasons.ESC) {
+            return 'by pressing ESC';
+        } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+            return 'by clicking on a backdrop';
+        } else {
+            return `with: ${reason}`;
+        }
+    }
+
+    newspopup(article: Article) {
+        const monthNames = ["January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        ];
+        this.clickedArticle = article;
+        this.twitterContent = "https://twitter.com/intent/tweet?text=" + encodeURI(article.title + article.articleUrl);
+        this.fbContent = "https://www.facebook.com/sharer/sharer.php?u=" + encodeURI(article.articleUrl);
+        let d = new Date(this.clickedArticle.date);
+        if (!!d.valueOf()) {
+            let year = d.getFullYear();
+            let month = d.getMonth();
+            let day = d.getDate();
+            this.clickedArticle.date = monthNames[month] + " " + day + ", " + year;
+        }
+        else {
+            this.clickedArticle.date = "";
+        }
+    }
+
+    buystockpopup() {
+        this.myControl.setValue(0);
+        this.myControl.valueChanges.subscribe(value => {
+            if (value > 0) {
+                this.stockbuyable = true;
+            }
+            else {
+                this.stockbuyable = false;
+            }
+            this.costprice = value * this.details.summaryModel.lastPrice;
+            this.costpriceStr = Utility.beautify(this.costprice);
+        });
+    }
+
+    buystock() {
+        this.portfolioService.buyStock(this.ticker,
+            this.myControl.value,
+            this.myControl.value * this.details.summaryModel.lastPrice,
+            this.details.outlookModel.companyName);
+        $('.buy-alert').show();
+        setTimeout(function () {
+            if ($('.buy-alert').length > 0) {
+                $('.buy-alert').hide();
+            }
+        }, 2000)
+    }
+
+    plotHighChart(stockInfoModel: StockInfoModel) {
+        if (stockInfoModel.parsing === false) {
+            console.log("Received parsing = false for historical data from backend");
+            return;
+        }
+
+        let info = stockInfoModel.data;
+
+        // split the data set into ohlc and volume
+        let ohlc = [];
+        let volume = [];
+        let dataLength = info.length;
+        for (let i = 0;i < dataLength;i++) {
+            ohlc.push([
+                info[i].date,
+                info[i].open,
+                info[i].high,
+                info[i].low,
+                info[i].close
+            ]);
+
+            volume.push([
+                info[i].date,
+                info[i].volume
+            ]);
+        }
+        // create the chart
+        this.chartOptions = {
+
+            rangeSelector: {
+                enabled: true,
+                allButtonsEnabled: true,
+                selected: 2
+            },
+
+            navigator: {
+                enabled: true,
+                adaptToUpdatedData: true
+            },
+
+            title: {
+                text: this.ticker + ' Historical'
+            },
+
+            subtitle: {
+                text: 'With SMA and Volume by Price technical indicators'
+            },
+
+            xAxis: {
+                type: 'datetime'
+            },
+
+            yAxis: [{
+                startOnTick: false,
+                endOnTick: false,
+                labels: {
+                    align: 'right',
+                    x: -3
+                },
+                title: {
+                    text: 'OHLC'
+                },
+                height: '60%',
+                lineWidth: 2,
+                resize: {
+                    enabled: true
+                }
+            }, {
+                labels: {
+                    align: 'right',
+                    x: -3
+                },
+                title: {
+                    text: 'Volume'
+                },
+                top: '65%',
+                height: '35%',
+                offset: 0,
+                lineWidth: 2
+            }],
+
+            tooltip: {
+                split: true
+            },
+
+            plotOptions: {
+                series: {
+                    dataGrouping: {
+                        units: [[
+                            'week',
+                            [1]
+                        ], [
+                            'month',
+                            [1, 2, 3, 4, 6]
+                        ]]
+                    }
+                }
+            },
+
+            series: [{
+                type: 'candlestick',
+                name: this.ticker,
+                id: this.ticker,
+                zIndex: 2,
+                data: ohlc
+            }, {
+                type: 'column',
+                name: 'Volume',
+                id: 'volume',
+                data: volume,
+                yAxis: 1
+            }, {
+                type: 'vbp',
+                linkedTo: this.ticker,
+                params: {
+                    volumeSeriesID: 'volume'
+                },
+                dataLabels: {
+                    enabled: false
+                },
+                zoneLines: {
+                    enabled: false
+                }
+            }, {
+                type: 'sma',
+                linkedTo: this.ticker,
+                zIndex: 1,
+                marker: {
+                    enabled: false
+                }
+            }]
+        };
+    }
 }
